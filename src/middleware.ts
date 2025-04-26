76b33ebdfd78205071bda7b7d95b8cc92d3b5562
s6352410016 <s6352410016@sau.ac.th>
2025-04-26 16:03:59 +0700

    fix access to order page

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,36 @@
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function middleware(request: NextRequest){
+export async function middleware(request: NextRequest) {
   const cookie = await cookies();
+  const pathname = request.nextUrl.pathname;
 
-  if(!cookie.get("delete-cart") && !cookie.get("enable")){
-    return NextResponse.redirect(new URL("/", request.url));
+  if (
+    pathname === "/auth-verify" ||
+    pathname === "/checkout" ||
+    pathname === "/create-order"
+  ) {
+    if (!cookie.get("delete-cart") && !cookie.get("enable")) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  } else if (pathname === "/order") {
+    if (!cookie.get("accessToken")) {
+      let prevPath = "";
+      const referer = request.headers.get("referer");
+      if (referer) {
+        const path = referer.split("/").pop();
+        !path
+          ? (prevPath = "/")
+          : path.includes("?")
+          ? (prevPath = "/")
+          : (prevPath = path);
+
+        return NextResponse.redirect(new URL(prevPath, request.url));
+      }
+    }
   }
 }
 
 export const config = {
-  matcher: ["/auth-verify", "/checkout", "/create-order"],
-}
+  matcher: ["/auth-verify", "/checkout", "/create-order", "/order"],
+};
